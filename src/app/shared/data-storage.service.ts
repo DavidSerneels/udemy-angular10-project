8edb5791bb58356babb903f51cb5d55370523690
David Serneels <david.serneels@cipalschaubroeck.be>
2020-10-23 11:57:40 +0200

    Section 19: add dataStorageService to save data on firebase, enable save data link in header

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {RecipeService} from '../body/recipes/recipe-service';
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+  private url = 'https://ng-course-recipe-book-ds.firebaseio.com/recipes.json';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(this.url, recipes)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+}
