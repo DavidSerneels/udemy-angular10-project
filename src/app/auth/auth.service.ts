92a33c028302afda1cffafafb4347aebbbe1eb03
David Serneels <david.serneels@cipalschaubroeck.be>
2020-10-23 13:48:38 +0200

    Section 20: create authService and signup method

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  private signupUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp';
+  private apiKey = 'AIzaSyDjK7U3svW-4j96uURx39FOoQ_xUU_35PU';
+
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string): Observable<AuthResponseData> {
+    return this.http
+      .post<AuthResponseData>(this.signupUrl, {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }, {
+        params: new HttpParams().set('key', this.apiKey)
+      });
+  }
+}
