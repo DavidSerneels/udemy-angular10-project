a21392be63617fc97b5211b2cdccbe5a610ff80f
David Serneels <david.serneels@cipalschaubroeck.be>
2020-10-23 16:32:46 +0200

    Section 20: add and implement an AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,11 @@ import {RecipeDetailComponent} from './body/recipes/recipe-detail/recipe-detail.
 import {RecipeEditComponent} from './body/recipes/recipe-edit/recipe-edit.component';
 import {RecipesResolverService} from './body/recipes/recipes-resolver.service';
 import {AuthComponent} from './auth/auth.component';
+import {AuthGuard} from './auth/auth.guard';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent, children: [
+  { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
       { path: '', component: RecipeStartComponent },
       { path: 'new', component: RecipeEditComponent },
       { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
