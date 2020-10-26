4c9c1797dc44ea51863398428a806195fe7de8cd
David Serneels <david.serneels@cipalschaubroeck.be>
2020-10-26 11:41:42 +0100

    Section 22: create and setup shoppingListModule

---
+++ src/app/body/shopping-list/shopping-list.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {ShoppingListRoutingModule} from './shopping-list-routing.module';
+import {ShoppingListComponent} from './shopping-list.component';
+import {ShoppingEditComponent} from './shopping-edit/shopping-edit.component';
+
+
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ],
+  imports: [
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    ShoppingListRoutingModule
+  ]
+})
+export class ShoppingListModule {
+
+}
