e0aade92e2bd4f4cb794dd9b277976138285cd7a
David Serneels <david.serneels@cipalschaubroeck.be>
2020-10-26 12:43:44 +0100

    Section 22: setup lazy loading for shopping list and auth modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: './body/recipes/recipes.module#RecipesModule'}
-  // { path: 'recipes', loadChildren: () => import('./body/recipes/recipes.module').then(module => module.RecipesModule)}
+  { path: 'auth', loadChildren: './auth/auth.module#AuthModule'},
+  { path: 'recipes', loadChildren: './body/recipes/recipes.module#RecipesModule'},
+  // { path: 'recipes', loadChildren: () => import('./body/recipes/recipes.module').then(module => module.RecipesModule)},
+  { path: 'shopping-list', loadChildren: './body/shopping-list/shopping-list.module#ShoppingListModule'},
 ];
 
 @NgModule({
--- src/app/body/shopping-list/shopping-list-routing.module.ts
+++ src/app/body/shopping-list/shopping-list-routing.module.ts
@@ -4,7 +4,7 @@ import {ShoppingListComponent} from './shopping-list.component';
 import {ShoppingEditComponent} from './shopping-edit/shopping-edit.component';
 
 const shoppingListRoutes: Routes = [
-  { path: 'shopping-list', component: ShoppingListComponent,
+  { path: '', component: ShoppingListComponent,
     children: [{ path: 'edit', component: ShoppingEditComponent}]}
 ];
 
